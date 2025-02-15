6be060a96ae2fea101baee55234cc206b9d6f96a
zonehisa <nijipapazoneokun123@gmail.com>
2025-02-15 16:33:44 +0900

    Initial Commit

---
+++ WeatherAndMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherAndMap
+//
+//  Created by 佐藤幸久 on 2025/02/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
