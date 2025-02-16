8f2503ade581cf9e962eaedffbd830ce6590b08e
zonehisa <nijipapazoneokun123@gmail.com>
2025-02-16 09:55:32 +0900

    レスポンスを取得する構造体と、新たなビューを作成していく

---
+++ WeatherAndMap/Models/ScreenInfo.swift
@@ -0,0 +1,8 @@
+//
+//  ScreenInfo.swift
+//  WeatherAndMap
+//
+//  Created by 佐藤幸久 on 2025/02/16.
+//
+
+import Foundation
---
+++ WeatherAndMap/Views/DailyWeatherView.swift
@@ -0,0 +1,18 @@
+//
+//  DailyWeatherView.swift
+//  WeatherAndMap
+//
+//  Created by 佐藤幸久 on 2025/02/16.
+//
+
+import SwiftUI
+
+struct DailyWeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DailyWeatherView()
+}
